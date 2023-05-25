4f180dfe21d98860fa1fb66e6850c5ebe5ec3be9
jainil <pate3862@sheridancollege.ca>
2023-05-25 09:56:07 -0400

    hii added

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -16,6 +16,7 @@ public class DemoGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hey jainil here");
+        System.out.println(" hi how are you");
     }
     
 }
